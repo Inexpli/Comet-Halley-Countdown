b996e83d164d631455a520ca29f10f234c6894f5
Jakub Jeż <kubajez02@gmail.com>
2020-12-16 21:20:09 +0100

    Upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const CometHalley = '28 July 2061';
+
+function countdown() {
+    const CometHalleyDate = new Date(CometHalley);
+    const currentDate = new Date();
+
+    const totalSeconds = (CometHalleyDate - currentDate) / 1000;
+
+    const years = Math.floor(totalSeconds/3600/24/365);
+    const days = Math.floor(totalSeconds / 3600 / 24)%365;
+    const hours = Math.floor(totalSeconds/3600) %24;
+    const minutes = Math.floor(totalSeconds/60) % 60;
+    const seconds = Math.floor(totalSeconds)%60;
+
+    console.log(days, hours, minutes, seconds);
+
+    document.getElementById("years").innerHTML = formatTime(years);
+    document.getElementById("days").innerHTML = formatTime(days);
+    document.getElementById("hours").innerHTML = formatTime(hours);
+    document.getElementById("minutes").innerHTML = formatTime(minutes);
+    document.getElementById("seconds").innerHTML = formatTime(seconds);
+    
+    function formatTime(time) {
+        return time < 10 ? `0${time}` : time;
+    }
+}
+
+setInterval(countdown, 1000);
+
+
+countdown();
